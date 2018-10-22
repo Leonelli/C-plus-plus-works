5a497fe6a6bada33981e548f3a56ac56dfab7ed6
Leonelli <matteoleonelli99@gmail.com>
2018-10-22 14:46:19 +0200

    pausa lezione

--- es_2018_10_22.cc
+++ es_2018_10_22.cc
@@ -25,6 +25,9 @@ int maxDiDieciDo(){
   return 0;
 }
 
+
+//Stampa n * a video e torna a capo
+// n numero intero acquisto da tastiera
 int asterisco(){
   int numero;
   cout << "Insceisci un numero intero: ";
@@ -36,10 +39,66 @@ int asterisco(){
   cout << endl;
   return 0;
 }
-//Stampa n * a video e torna a capo
-// n numero intero acquisto da tastiera
+
+//stampa n asterischi a video uno sotto l'altro
+int asteriscoCapo(){
+  int numero;
+  cout << "Insceisci un numero intero: ";
+  cin >> numero;
+
+  for (int i = 0; i < numero; i++) {
+    cout << "* "<< endl;
+  }
+  return 0;
+}
+
+int asteriscoDiagonale(){
+  int numero;
+  cout << "Insceisci un numero intero: ";
+  cin >> numero;
+
+  for (int i = 0; i < numero; i++) {
+    for (int j=0; j < i; j++) {
+      cout << "- ";
+    }
+    cout << "* "<< endl;
+  }
+  return 0;
+}
+
+int asteriscoDiagonaleContraria(){
+  int numero;
+  cout << "Insceisci un numero intero: ";
+  cin >> numero;
+
+  for (int i = numero; i > 0; i--) {
+    for (int j=i-1; j > 0; j--) {
+      cout << "- ";
+    }
+    cout << "* "<< endl;
+  }
+  return 0;
+}
+
+//dato m il valore di una base e n il valore di un esponenete
+// calcolare m elevato alla n (interi)
+int potenza(){
+  int m,n;
+  int risultato=1;
+  cout<<"inserire base: ";
+  cin >> m;
+  cout<<"inserire esponente: ";
+  cin >> n;
+
+  for (int i = 0; i < n; i++) {
+    risultato*=m;
+  }
+  cout << m << "^" << n << " = " << risultato << endl;
+  return 0;
+}
+
 
 int main() {
-  int max = asterisco();
+  int max = potenza();
   return 0;
 }
