2d328ec81040bc47aa78f758da5165a15fd642c8
Leonelli <matteoleonelli99@gmail.com>
2018-10-29 16:03:09 +0100

    mcm

--- es_2018_10_29.cc
+++ es_2018_10_29.cc
@@ -11,6 +11,8 @@ int asteriscoCapo(int numero);
 int asteriscoDiagonale(int numero);
 int asteriscoDiagonaleContraria(int numero);
 int massimo(int,int);
+int mcm(int, int);
+
 int main() {
   /*int limite;
   cout << "Inserisci un numero di elementi: ";
@@ -35,18 +37,26 @@ int main() {
     case 3: asteriscoDiagonale(numero);
       break;
     case 4: asteriscoDiagonaleContraria(numero);
-      break;
+      break;int mcm(int n1, int n2)
     default:
       cout<<"Non hai insetito una scelta corretta" << endl;
   }*/
 
-  int n1,n2,max;
+  /*int n1,n2,max;
   cout << "Inserire un reale: ";
   cin >> n1;
   cout << "Inserire un reale: ";
   cin >> n2;
   max = massimo(n1,n2);
-  cout << "Il massimo è: " <<max <<endl; 
+  cout << "Il massimo è: " <<max <<endl;*/
+
+  int n1,n2,mincommult;
+  cout << "Inserire un reale: ";
+  cin >> n1;
+  cout << "Inserire un reale: ";
+  cin >> n2;
+  mincommult = mcm(n1,n2);
+  cout << "Il mcm è: " <<mincommult <<endl;
   return 0;
 }
 
@@ -132,4 +142,45 @@ int massimo(int n1, int n2){
     max=n2;
   }
   return max;
+}
+
+
+//acquisire da tastiera due numeri interi,
+//calcolare il minimo comune multiplo di qusti due interi attraverso una funzione
+//che prende in input due interi e restituisce il minimo comune multiplo
+//il minimo comune multiplo è il numero più piccolo che è mulitplo di tutti e due i numeri senza resto
+int mcm(int n1, int n2){
+
+  int mincommult = n1*n2;
+  if (n1%n2==0 ) {
+    return n1;
+  }
+  else if(n2%n1==0){
+    return n2;
+  }
+  int candidato= mincommult
+  else{
+    for (int i = mincommult ; i >= 0; i--) {
+    cout <<n1<<" " << n2 <<" " << i << endl;
+      if (i%n1==0 && i%n2==0) {
+        candidato = i;
+      }
+    }
+  }
+  return 0;
+}
+
+int mcmProf(int a, int b){
+  int candidato,i;
+
+   candidato=a*b;
+   i = candidato;
+   max = massimo(a,b);
+   while (i>max) {
+     if ((i%a==0) && (i%b==0)) {
+       candidato = i;
+     }
+     i--;
+   }
+   return candidato;
 }
