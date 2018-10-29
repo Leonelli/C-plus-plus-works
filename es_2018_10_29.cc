226d675c3b20ebae64136adcb64a9471adb7fc1d
Leonelli <matteoleonelli99@gmail.com>
2018-10-29 13:25:26 +0100

    esercitazione 29 ottobre

---
+++ es_2018_10_29.cc
@@ -0,0 +1,34 @@
+using namespace std;
+#include <iostream>
+#include <cmath>
+
+int funzione1{
+
+  return 0;
+}
+
+
+int funzione1{
+
+  return 0;
+}
+
+int funzione1{
+
+  return 0;
+}
+
+int funzione1{
+
+  return 0;
+}
+
+int funzione1{
+
+  return 0;
+}
+
+int main() {
+  int funzione = fibonacci2();
+  return 0;
+}
