bec6788f39c4428c7ae822ca1137ff4ae3677c1b
Leonelli <matteoleonelli99@gmail.com>
2018-12-03 16:02:36 +0100

    stringhe

---
+++ laboratorio/es_2018_12_03_3.cc
@@ -0,0 +1,38 @@
+using namespace std;
+#include <iostream>
+#include <cstring>
+
+//echo stringa | ./a.out
+
+const int LUNGHEZZA = 256;
+
+void codifica(char t[], const char s[]);
+char caesar(char c);
+
+
+int main(int argc, char* argv[]){
+  char sorgente[LUNGHEZZA], destinazione[LUNGHEZZA];
+  cin >> sorgente;
+  codifica(destinazione,sorgente);
+  cout <<"sorgente: \t"<< sorgente << endl;
+  cout << "destinazione : \t" << destinazione << endl;
+  return 0;
+}
+
+void codifica(char t[], const char s[]){
+  int i;
+  for (i = 0; i < strlen(s); i++) {
+    t[i]=caesar(s[i]);
+  }
+  t[i]='\0';
+}
+char caesar(char c){
+    if (isalpha(c)) {
+      if (islower(c)) {
+        c=(c - 'a' + 3) % ('z' - 'a') + 'a';
+      }
+      else{
+        c=(c - 'A' + 3) % ('z' - 'a') + 'A';
+      }
+  return c;
+}
---
+++ laboratorio/es_2018_12_03_5.cc
@@ -0,0 +1,42 @@
+using namespace std;
+#include <iostream>
+#include <cstring>
+
+//echo stringa | ./a.out
+
+const int LUNGHEZZA = 256;
+
+void codifica(char t[], const char s[]);
+char caesar(char c);
+
+
+int main(int argc, char* argv[]){
+  char sorgente[LUNGHEZZA], destinazione[LUNGHEZZA];
+  cin.getline(sorgente,LUNGHEZZA);
+  codifica(destinazione,sorgente);
+  cout <<"sorgente: \t"<< sorgente << endl;
+  cout << "destinazione : \t" << destinazione << endl;
+  return 0;
+}
+
+void codifica(char t[], const char s[]){
+  if(strlen(s)==0){
+    strcat(t,"");
+  }
+  else{
+    char tmp[2] = {caesar(s[0], '\0')};
+    strcat(t, tmp);
+    codifica(t,s+1);
+  }
+}
+char caesar(char c){
+    if (isalpha(c)) {
+      if (islower(c)) {
+        c=(c - 'a' + 3) % ('z' - 'a') + 'a';
+      }
+      else{
+        c=(c - 'A' + 3) % ('z' - 'a') + 'A';
+      }
+    }
+  return c;
+}
