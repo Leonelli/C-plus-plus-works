fcff8b26e694f31e51301d094997bd2b497a01db
Leonelli <matteoleonelli99@gmail.com>
2018-12-17 15:51:43 +0100

    fix giardino

--- laboratorio/es_2018_12_17_00.cc
+++ laboratorio/es_2018_12_17_00.cc
@@ -65,8 +65,8 @@ void riempiGiardino(Albero ***giardino, int dimX, int dimY,int *quanti)
   srand (time(NULL));
 
   for (int i = 0; i < quanti[0]; i++) {
-    randx = rand() % dimX + 1;
-    randy = rand() % dimY + 1;
+    randx = rand() % dimX;
+    randy = rand() % dimY;
 
     Albero * tmp = new Albero;
     tmp->nome_specie=ABETE;
@@ -75,58 +75,73 @@ void riempiGiardino(Albero ***giardino, int dimX, int dimY,int *quanti)
   }
 
   for (int i = 0; i < quanti[1]; i++) {
-    randx = rand() % dimX + 1;
-    randy = rand() % dimY + 1;
+    randx = rand() % dimX;
+    randy = rand() % dimY;
 
+    if(giardino[randx][randy]== NULL){
     Albero * tmp = new Albero;
     tmp->nome_specie=PINO;
     tmp->numero_identificativo=i;
     giardino[randx][randy]= tmp;
   }
+  else{
+    i--;
+  }
+  }
 
   for (int i = 0; i < quanti[2]; i++) {
-    randx = rand() % dimX + 1;
-    randy = rand() % dimY + 1;
+    randx = rand() % dimX;
+    randy = rand() % dimY;
+
+    if(giardino[randx][randy]== NULL){
 
     Albero * tmp = new Albero;
     tmp->nome_specie=QUERCIA;
     tmp->numero_identificativo=i;
     giardino[randx][randy]= tmp;
   }
+  else{
+    i--;
+  }
+  }
 
   for (int i = 0; i < quanti[3]; i++) {
-    randx = rand() % dimX + 1;
-    randy = rand() % dimY + 1;
-
+    randx = rand() % dimX;
+    randy = rand() % dimY;
+    if(giardino[randx][randy]== NULL){
     Albero * tmp = new Albero;
     tmp->nome_specie=FAGGIO;
     tmp->numero_identificativo=i;
     giardino[randx][randy]= tmp;
   }
+  else{
+    i--;
+  }
+  }
 
   for (int i = 0; i < quanti[4]; i++) {
-    randx = rand() % dimX + 1;
-    randy = rand() % dimY + 1;
+    randx = rand() % dimX;
+    randy = rand() % dimY;
 
-    //if(giardino[randx][randy]!=NULL){
-      Albero * tmp = new Albero;
-      tmp->nome_specie=SALICE;
-      tmp->numero_identificativo=i;
-      giardino[randx][randy]= tmp;
-    //}
-    //else{
-    //  i--;
-    //}
+    if(giardino[randx][randy]== NULL){
+    Albero * tmp = new Albero;
+    tmp->nome_specie=SALICE;
+    tmp->numero_identificativo=i;
+    giardino[randx][randy]= tmp;
+  }
+  else{
+    i--;
+  }
   }
 
   for (int i = 0; i < dimX; i++) {
     for (int j = 0; j < dimY; j++) {
       if (giardino[i][j]!=NULL) {
-        cout << giardino[i][j] -> nome_specie<< "\t";
+        cout << giardino[i][j] -> numero_identificativo<< "\t";
       }
-      /*else{
-        cout << "/t";
-      }*/
+      else{
+        cout << "-\t";
+      }
     }
     cout << endl;
   }
